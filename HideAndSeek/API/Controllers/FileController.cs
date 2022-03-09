f48783c74719d59157acb357f4953dcf9203e317
emiliaolteanu13 <emiliaolteanu13@yahoo.com>
2022-03-09 14:23:31 +0200

    encrypt/decrypt file

--- HideAndSeek/API/Controllers/FileController.cs
+++ HideAndSeek/API/Controllers/FileController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using HideAndSeek.Utils;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace HideAndSeek.API.Controllers
 {
@@ -6,30 +9,37 @@ namespace HideAndSeek.API.Controllers
     [ApiController]
     public class FileController : ControllerBase
     {
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
         [HttpPost("import")]
         public IActionResult ImportFile([FromForm] IFormFile file, [FromForm] string key, [FromForm] string operation)
         {
             string name = file.FileName;
-            string extension = Path.GetExtension(file.FileName);
+            string extension = Regex.Match(name, @"\..*").Value;
+            string fileName = name.Replace(extension, "");
             byte[] fileToBytes;
+            byte[] keyToBytes = Encoding.ASCII.GetBytes(key);
             using (var memoryStream = new MemoryStream())
             {
                 file.CopyTo(memoryStream);
+
                 fileToBytes = memoryStream.ToArray();
-                extension += ".enc";
+                if(operation=="encrypt")
+                    extension += ".enc";
+                else
+                {
+                    int n = extension.Split('.').Length;
+                    if (extension.Split('.').Length > 2)
+                        extension = extension.Replace(".enc", "");
+                    else
+                        extension += ".dec";
+                }
             }
+            byte[] encryptedDataBytes = Cryptographer.EncryptDecrypt(fileToBytes, keyToBytes);
+            string folderName = @".\EncryptedDataStore";
+            fileName += extension;
+            string pathString = System.IO.Path.Combine(folderName, fileName);
+            
+            System.IO.File.WriteAllBytes(pathString, encryptedDataBytes);
+               
             return Ok(fileToBytes);
         }
 
---
+++ HideAndSeek/API/Utils/Cryptographer.cs
@@ -0,0 +1,24 @@
+﻿namespace HideAndSeek.Utils
+{
+    public static class Cryptographer
+    {
+        
+            public static byte[] EncryptDecrypt(byte[] data, byte[] key)
+            {
+                if (data == null)
+                    throw new ArgumentNullException(nameof(data));
+
+                if (key == null)
+                    throw new ArgumentNullException(nameof(key));
+
+                if (key.Length == 0)
+                    throw new ArgumentOutOfRangeException("key cannot be empty");
+
+                for (int i = 0; i < data.Length; i++)
+                    data[i] = (byte)(data[i] ^ key[i % key.Length]);
+
+                return data;
+            }
+        
+    }
+}
